a6bce8c12a309d12ae3c53a4136440109e9367a1
Obiora Okwubanego <jtobiora@gmail.com>
2019-06-19 11:24:19 +1000

    pushed cms backend

--- src/main/java/com/unionsystems/model/Customer.java
+++ src/main/java/com/unionsystems/model/Customer.java
@@ -22,60 +22,6 @@ public class Customer {
     private String phoneNumber;
     private String address;
 
-//    public Customer() {
-//
-//    }
-//
-//    public Customer(String firstName, String lastName, String email) {
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.email = email;
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPhoneNumber() {
-//        return phoneNumber;
-//    }
-//
-//    public void setPhoneNumber(String phoneNumber) {
-//        this.phoneNumber = phoneNumber;
-//    }
-//
-//    public String getAddress() {
-//        return address;
-//    }
-//
-//    public void setAddress(String address) {
-//        this.address = address;
-//    }
-
     @Override
     public String toString() {
         return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
