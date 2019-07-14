d5faf0d22a15d8cf487793cc1ec5aae5553fac7d
Obiora Okwubanego <jtobiora@gmail.com>
2019-07-14 20:46:57 +1000

    first commit

--- src/main/java/com/unionsystems/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unionsystems.repository;
-
-import com.unionsystems.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer,Long>{
-}
